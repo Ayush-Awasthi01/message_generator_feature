1ff9fc431510cb375bf579a84fbeb4971350aa2e
Ayush-Awasthi01 <ayushawasthi765@gmail.com>
2025-09-30 21:38:42 +0530

    Remove secrets and add .gitignore

--- app.py
+++ app.py
@@ -71,4 +71,5 @@ def generate_message():
     return jsonify({"error": "Invalid mode"}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
